2a013bf64d2b44572efd6ccd9fd6ecc9622efca1
StudentTest <75246020+RajaniGowrisetti@users.noreply.github.com>
2021-02-23 02:40:41 +0530

    Update Block.js

--- js/Block.js
+++ js/Block.js
@@ -7,13 +7,17 @@ class Block{
             //isStatic:true
            
         }
+        this.visibility = 225;
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
-        this.Visiblity = 255;
         World.add(world, this.body);
+        
       }
+     
       display(){
+        console.log(this.body.speed);
+        if(this.body.speed <3){
         var angle = this.body.angle;
         var pos= this.body.position;
         push();
@@ -22,16 +26,12 @@ class Block{
         rectMode(CENTER);
         rect(0,0,this.width, this.height);
         pop();
-
-        if(this.body.speed<3){
-          this.body
-        }
-        else{
-          World.remove(world,this.body);
-          push();
-          this.Visiblity = this.Visiblity - 5;
-          tint(255,this.Visiblity);
-          pop();
-        }
+      }else{
+        World.remove(world, this.body);
+        push();
+        this.visibility = this.visibility -5;
+        pop();
+        
       }
+    }
 }
